e4c7d15a9db4a6b362614914b315a87025005428
Scott Severance <scott@scottseverance.us>
2021-06-25 14:26:33 -0500

    exercise 2 complete

--- main.js
+++ main.js
@@ -17,10 +17,22 @@ window.onload = () => {
 
 // TODO: Create a paragraph element using this element.createElement() and put this text inside "This node was created using the createElement() method"
 // TODO: Append the created node to the parent node using the element.appendChild() method
-
 // TODO: Create a <a> element using this element.createElement() and put this text inside "I am a <p> tag"
 // TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
 // BONUS: Add a link href to the <a>
+
+let p = document.createElement('p');
+p.innerHTML = 'This node was created using the <code>createElement()</code> method<br>';
+let a = document.createElement('a');
+//a.link = 'https://www.scottseverance.us'; // Instructions are wrong. This doesn't work.
+a.setAttribute('href', 'https://www.scottseverance.us');
+a.innerText = 'My website';
+let parent = document.getElementById('parent');
+
+setTimeout(() => {parent.appendChild(p);}, 2000);
+setTimeout(() => {parent.insertBefore(a, p)}, 2500);
+
+
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
 
